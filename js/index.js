d9fc19a2cbad3d7161d62c5e81e6ff161a1b26d2
Dan Savidge <dsavidge02@gmail.com>
2024-06-20 14:51:51 -0400

    fixed conflict from not having dustyBucket pulled

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 function getDuck() {
   const duckDiv = document.getElementById("duck");
- const duckDiv = document.getElementById('duck');
   duckDiv.innerHTML = `🦆
   <button onclick="cookDuck()">Cook Duck</button>`;
 }
 
 function cookDuck() {
-  const duckDiv = document.getElementById('duck');
+  const duckDiv = document.getElementById("duck");
 
   // console.log("ran the cookDuck function.");
 
@@ -17,4 +16,3 @@ function getDev() {
   const devDiv = document.getElementById("dev");
   devDiv.innerHTML = "🛠️";
 }
-  
