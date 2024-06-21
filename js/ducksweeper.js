c85a2360fd651aef77bca60480085f01138328f9
Angie Chen <alice@webage.com>
2024-06-21 15:19:06 -0400

    event handlings for table working

--- js/ducksweeper.js
+++ js/ducksweeper.js
@@ -1,6 +1,8 @@
 // find all the ducks by removing ALL surrounding grass! be careful not to click on any before finding them all, or else they will fly away!
-let board = [[]];
-let visited = [[]];
+const board = [[]];
+const visited = [[]];
+const gameTable = document.getElementById("ducksweeperGameBoard");
+const gameInfo = document.getElementById("gameInfo");
 
 function fillEmptyMap(array, width, height) {
   for (var x = 0; x < width; x++) {
@@ -38,9 +40,50 @@ function populateBoard(numDucks) {
     }
   }
 }
-// function prettyPrint(b) {}
+function numberDFS(x, y) {
+  let localVisited = [[]];
+  fillEmptyMap(localVisited, 10, 10);
+  //   const stat
+  const stack = [[x, y]];
+
+  while (stack.length) {
+    const vertex = 
+  }
+}
+function clickHandler(event) {
+  let arr = event.target.id.split(",").map(Number);
+  let x = arr[0];
+  let y = arr[1];
+
+  console.log(arr, board[x][y]);
+  if (visited[x][y] == 1) return;
+  else visited[x][y] = 1;
+
+  if (board[x][y] === -1) {
+    // game over
+    gameInfo.innerText = "Oh no! You scared away the ducks :(";
+    // display entire board
+    // change new game to play again
+  } else if (board[x][y] === 0) {
+    // clear all 0 and adjacent numbers to 0
+  } else {
+    let cell = document.getElementById(event.target.id);
+    cell.innerHTML = board[x][y];
+    console.log("yuh");
+  }
+}
+
 fillEmptyMap(board, 10, 10);
 fillEmptyMap(visited, 10, 10);
-
 populateBoard(10);
-console.log(board);
+
+for (let i = 0; i < 10; i++) {
+  let row = gameTable.insertRow(i);
+  for (let j = 0; j < 10; j++) {
+    let cell = row.insertCell(j);
+    // cell.innerHTML = board[i][j]; // debug only
+    cell.innerHTML = "[]";
+    cell.id = `${i},${j}`;
+    cell.addEventListener("click", (e) => clickHandler(e));
+  }
+}
