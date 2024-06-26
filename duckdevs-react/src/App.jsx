d82687105f342532ecb1518942a59689ac0eeee5
Chris Minnick <chris@minnick.com>
2024-06-26 10:39:16 -0400

    fixed merge errors

--- duckdevs-react/src/App.jsx
+++ duckdevs-react/src/App.jsx
@@ -1,14 +1,14 @@
-import "./App.css";
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import People from "./components/People";
-import people_data from "./assets/data/duck_devs_info.json";
-import Leaderboard from "./components/Leaderboard";
-import DuckArmy from "./components/DuckArmy";
-import DuckPond from "./components/DuckPond.jsx";
-import DuckSweeper from "./components/DuckSweeper";
-import DuckRacer from "./components/DuckRacer";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import People from './components/People';
+import people_data from './assets/data/duck_devs_info.json';
+import Leaderboard from './components/Leaderboard';
+import DuckArmy from './components/DuckArmy';
+import DuckPond from './components/DuckPond.jsx';
+import DuckSweeper from './components/DuckSweeper';
+import DuckRacer from './components/DuckRacer';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
@@ -29,15 +29,4 @@ function App() {
   );
 }
 
-function DuckPond(props) {
-  return (
-    <div style={{ width: '300px', height: '300px', border: '1px solid blue' }}>
-      Welcome to the duck pond<div>{props.children}</div>
-    </div>
-  );
-}
-
-function Duck() {
-  return 'quack';
-}
 export default App;
