d6384ae4124c35657d2098f415c2a2c5311ae2f5
Chris Minnick <chris@minnick.com>
2024-06-28 14:28:37 -0400

    added server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const { createServer } = require('node:http');
+
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at Port ${port}/`);
+});
